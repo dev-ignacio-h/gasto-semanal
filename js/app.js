1150e69d951ec1f73f51b8aa5738b33caf7b23ee
Ignacio <dev.ignacio.h@gmail.com>
2021-02-06 16:02:03 -0300

    se añaden funcionalidades a la clase UI

--- js/app.js
+++ js/app.js
@@ -1,39 +1,51 @@
 // variables y selectores
-const formulario = document.querySelector('#agregar-gasto')
-const gastoListado = document.querySelector('#gastos ul')
+const formulario = document.querySelector("#agregar-gasto");
+const gastoListado = document.querySelector("#gastos ul");
 
 // eventos
 
 eventlisteners();
 function eventlisteners() {
-    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+  document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
 }
 
 // clases
 class Presupuesto {
-    constructor(presupuesto) {
-        this.presupuesto = +presupuesto
-        this.restante = +presupuesto
-        this.gastos = []
-    }
+  constructor(presupuesto) {
+    this.presupuesto = +presupuesto;
+    this.restante = +presupuesto;
+    this.gastos = [];
+  }
 }
 
 class UI {
-
+  insertarPresupuesto(cantidad) {
+    // Se extrae el valor
+    const { presupuesto, restante } = cantidad;
+    // Se agrega al html
+    document.querySelector('#total').textContent = presupuesto;
+    document.querySelector('#restante').textContent = restante;
+  }
 }
 
+// Instanciar
+const ui = new UI();
 let presupuesto; // objeto que se instanciará luego
-
 
 // funciones
 function preguntarPresupuesto() {
-    const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?')
-    // console.log(+presupuestoUsuario);
-    if(!presupuestoUsuario || isNaN(presupuestoUsuario) || presupuestoUsuario <=0) {
-        window.location.reload();
-    }
+  const presupuestoUsuario = prompt("¿Cuál es tu presupuesto?");
+  // console.log(+presupuestoUsuario);
+  if (
+    !presupuestoUsuario ||
+    isNaN(presupuestoUsuario) ||
+    presupuestoUsuario <= 0
+  ) {
+    window.location.reload();
+  }
 
-    // Presupuesto valido
-    presupuesto = new Presupuesto(presupuestoUsuario);
-    console.log(presupuesto);
+  // Presupuesto valido
+  presupuesto = new Presupuesto(presupuestoUsuario);
+
+  ui.insertarPresupuesto(presupuesto);
 }
